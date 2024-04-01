a7e3ee38c1a9432fcee83d2fde1fc8a3e7584d6e
Akewich <ekawichgm@gmail.com>
2024-04-01 19:03:27 +0700

    fixed error

--- Vista/IndexPage/src/App.tsx
+++ Vista/IndexPage/src/App.tsx
@@ -1,13 +1,11 @@
 import "./App.css";
-<<<<<<< Updated upstream
 import Build from "./components/ContentConponents/Build";
 import Database from "./components/ContentConponents/Database";
 import Detail from "./components/ContentConponents/Detail";
 import Footer from "./components/ContentConponents/Footer";
 import Home from "./components/ContentConponents/Home";
 import Service from "./components/ContentConponents/Service";
-import LoginPage from "./components/LoginPage/LoginPage";
-import SignUp from "./components/LoginPage/SignUp";
+import { useState } from "react";
 
 function App() {
   const [isLoading,setIsLoading] = useState(true) 
