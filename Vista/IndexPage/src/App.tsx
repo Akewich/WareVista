24e2c3879b3d0269178dd4b2fc47d5201aa823e0
Akewich <ekawichgm@gmail.com>
2024-04-02 00:02:55 +0700

    Do  docker
    make preview docker image

--- Vista/IndexPage/src/App.tsx
+++ Vista/IndexPage/src/App.tsx
@@ -5,17 +5,15 @@ import Detail from "./components/ContentConponents/Detail";
 import Footer from "./components/ContentConponents/Footer";
 import Home from "./components/ContentConponents/Home";
 import Service from "./components/ContentConponents/Service";
-import { useState } from "react";
 
 function App() {
-  const [isLoading,setIsLoading] = useState(true) 
   return (
     <>
       <Home />
       <Service />
       <Detail />
       <Database />
-      <Build /> 
+      <Build />
       <Footer />
     </>
   );
--- Vista/IndexPage/vite.config.ts
+++ Vista/IndexPage/vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  preview: {
+  preview:{
     host: true,
-    port:8000
+    strictPort:true,
+    port:8080
   }
+  
 })
