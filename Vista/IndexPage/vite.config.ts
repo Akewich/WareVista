30e41df3aed3edbec958f96b7719085c87b391b3
Akewich <ekawichgm@gmail.com>
2024-04-09 00:11:34 +0700

    change docker file
    change Dockerfile and Docker compose

--- Vista/IndexPage/vite.config.ts
+++ Vista/IndexPage/vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview:{
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server:{
     host: true,
     strictPort: true,
     port: 8080
